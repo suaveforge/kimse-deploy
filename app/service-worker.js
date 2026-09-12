c6216db048b8c384dd0d3d466419c6742c02b100
programmer119 <53187583+programmer119@users.noreply.github.com>
2026-09-12 11:52:45 +0900

    Sync KIMSE app from source: app/service-worker.js

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'kimse-pwa-20260912-remote01';
+const CACHE = 'kimse-pwa-20260912-p0';
 const CORE = ['./','./index.html','./app.css','./app.js','./manifest.webmanifest'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
