8942620d22fd579c4e79a0554d2f6e7ed96231c3
programmer119 <53187583+programmer119@users.noreply.github.com>
2026-09-12 11:06:22 +0900

    Deploy KIMSE P0 remote preview: app/service-worker.js

---
+++ app/service-worker.js
@@ -0,0 +1,14 @@
+const CACHE = 'kimse-pwa-20260912-remote01';
+const CORE = ['./','./index.html','./app.css','./app.js','./manifest.webmanifest'];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting())));
+self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin) return;
+  event.respondWith(fetch(event.request).then(resp => {
+    const copy = resp.clone();
+    caches.open(CACHE).then(c => c.put(event.request, copy));
+    return resp;
+  }).catch(() => caches.match(event.request).then(r => r || caches.match('./index.html'))));
+});
