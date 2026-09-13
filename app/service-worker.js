d69617b4aca45abc47d29320110215a36f6050ef
programmer119 <53187583+programmer119@users.noreply.github.com>
2026-09-13 18:49:23 +0900

    Deploy KIMSE Release 03 PWA icon set

--- app/service-worker.js
+++ app/service-worker.js
@@ -1,5 +1,16 @@
-const CACHE = 'kimse-pwa-20260912-release02';
-const CORE = ['./','./index.html','./app.css','./app.js','./manifest.webmanifest','./assets/icons/icon.svg'];
+const CACHE = 'kimse-pwa-20260913-release03';
+const CORE = [
+  './',
+  './index.html',
+  './app.css',
+  './app.js',
+  './manifest.webmanifest',
+  './assets/icons/icon-192.png',
+  './assets/icons/icon-512.png',
+  './assets/icons/icon-maskable-512.png',
+  './assets/icons/apple-touch-icon-180.png',
+  './assets/icons/favicon-32.png'
+];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting())));
 self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', event => {
